4585bfa1c2a19eb831c204531d1ffa4c1db7ae1b
Adithyan k P <adithyankp777@gmail.com>
2022-02-12 22:00:04 +0530

    Navbar updation

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,12 +3,13 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <div className="navbar">
-      <img
+      {/* <img
         className="logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png
 "
         alt="Netflix logo"
-      />
+      /> */}
+      <h1 style={{ color: "red", fontFamily: "sans-serif" }}>ADIFLIX</h1>
       <img
         className="avatar"
         src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
